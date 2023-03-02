538bf26890a6489f807aeae17c5a2f3accc6f1a6
Aurélien Deharbe <aurelien.deharbe@ssi.gouv.fr>
2023-03-02 11:38:24 +0100

    add diode-flood-test binary to repeat sending a random buffer

---
+++ src/bin/diode-flood-test.rs
@@ -0,0 +1,56 @@
+use clap::{Arg, Command};
+use rand::RngCore;
+use std::io::Write;
+use std::net::TcpStream;
+use std::{env, net::SocketAddr, str::FromStr};
+
+fn main() {
+    let args = Command::new(env!("CARGO_BIN_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::new("to_tcp")
+                .long("to_tcp")
+                .value_name("ip:port")
+                .default_value("127.0.0.1:5000")
+                .help("Address and port to connect to diode-send"),
+        )
+        .arg(
+            Arg::new("buffer_size")
+                .long("buffer_size")
+                .value_name("nb_bytes")
+                .default_value("4194304") // 4096 * 1024
+                .value_parser(clap::value_parser!(usize))
+                .help("Size of file read/TCP write buffer"),
+        )
+        .get_matches();
+
+    let to_tcp = SocketAddr::from_str(args.get_one::<String>("to_tcp").expect("default"))
+        .expect("invalid to_tcp parameter");
+    let buffer_size = *args.get_one::<usize>("buffer_size").expect("default");
+
+    init_logger();
+
+    log::debug!("connecting to {}", to_tcp);
+    let mut diode = TcpStream::connect(to_tcp).expect("tcp connect");
+    diode
+        .shutdown(std::net::Shutdown::Read)
+        .expect("tcp read shutdown");
+
+    let mut thread_rng = rand::thread_rng();
+    let mut buffer = vec![0; buffer_size];
+    thread_rng.fill_bytes(&mut buffer);
+
+    loop {
+        log::debug!("sending buffer of {buffer_size} bytes");
+        diode.write_all(&buffer).expect("tcp write");
+    }
+}
+
+fn init_logger() {
+    if env::var("RUST_LOG").is_ok() {
+        simple_logger::init_with_env()
+    } else {
+        simple_logger::init_with_level(log::Level::Info)
+    }
+    .expect("logger initialization")
+}
