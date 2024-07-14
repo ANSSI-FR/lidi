b1d17536af060d3431b3af9b89283bb7bdaa19c0
Arnaud Fontaine <33485997+github-af@users.noreply.github.com>
2024-07-14 12:22:44 +0200

    diode-{send,receive}-udp: stream UDP datagrams (a single stream)

---
+++ src/aux/udp/send.rs
@@ -0,0 +1,49 @@
+use crate::aux::{self, udp};
+use std::{
+    io::{Read, Write},
+    net,
+    os::unix,
+};
+
+fn send_udp_aux<D>(
+    config: &udp::Config<aux::DiodeSend>,
+    mut diode: D,
+    from_udp: net::SocketAddr,
+) -> Result<(), udp::Error>
+where
+    D: Read + Write,
+{
+    let mut buffer = vec![0; config.buffer_size];
+
+    log::info!("binding UDP socket to {from_udp}");
+
+    let socket = net::UdpSocket::bind(from_udp)?;
+
+    loop {
+        let (size, _) = socket.recv_from(&mut buffer)?;
+
+        log::trace!("received datagram of {size} bytes");
+
+        let header = udp::protocol::Header { size };
+        header.serialize_to(&mut diode)?;
+        diode.write_all(&buffer[..size])?;
+    }
+}
+
+pub fn send(
+    config: &udp::Config<aux::DiodeSend>,
+    from_udp: net::SocketAddr,
+) -> Result<(), udp::Error> {
+    log::info!("connecting to {}", config.diode);
+
+    match &config.diode {
+        aux::DiodeSend::Tcp(socket_addr) => {
+            let diode = net::TcpStream::connect(socket_addr)?;
+            send_udp_aux(config, diode, from_udp)
+        }
+        aux::DiodeSend::Unix(path) => {
+            let diode = unix::net::UnixStream::connect(path)?;
+            send_udp_aux(config, diode, from_udp)
+        }
+    }
+}
