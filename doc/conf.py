97f40b6f4c5caa686deb5c9d2364ec83f711a5fc
sn-anssi <stephane.neveu@ssi.gouv.fr>
2025-03-26 07:59:29 +0100

    Documentation: Bump release to 1.3.5

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = 'Lidi'
 copyright = '2023, ANSSI-FR'
 author = 'ANSSI-FR'
-release = '1.3.4'
+release = '1.3.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
