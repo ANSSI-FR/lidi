628cd64e2388f3b064032468460014ed4c266180
sn-anssi <stephane.neveu@ssi.gouv.fr>
2025-03-26 07:57:20 +0100

    Clippy: Change returning errors using io::Error::other()

--- src/sock_utils.rs
+++ src/sock_utils.rs
@@ -12,17 +12,18 @@ pub fn set_socket_recv_buffer_size<S: AsRawFd>(socket: &S, size: i32) -> Result<
 }
 
 unsafe fn setsockopt_buffer_size(fd: i32, size: i32, option_name: i32) -> Result<(), io::Error> {
-    let res = libc::setsockopt(
+    let res = unsafe { libc::setsockopt(
         fd,
         libc::SOL_SOCKET,
         option_name,
         ptr::addr_of!(size).cast::<libc::c_void>(),
         mem::size_of::<libc::c_int>() as libc::socklen_t,
-    );
+    ) };
+    
     if res == 0 {
         Ok(())
     } else {
-        Err(io::Error::new(io::ErrorKind::Other, "libc::setsockopt"))
+        Err(io::Error::other("libc::setsockopt"))
     }
 }
 
@@ -37,16 +38,16 @@ pub fn get_socket_recv_buffer_size<S: AsRawFd>(socket: &S) -> Result<i32, io::Er
 unsafe fn getsockopt_buffer_size(fd: i32, option_name: i32) -> Result<i32, io::Error> {
     let mut sz = 0i32;
     let mut len = mem::size_of::<libc::c_int>() as libc::socklen_t;
-    let res = libc::getsockopt(
+    let res = unsafe { libc::getsockopt(
         fd,
         libc::SOL_SOCKET,
         option_name,
         ptr::addr_of_mut!(sz).cast::<libc::c_void>(),
         &mut len,
-    );
+    ) };
     if res == 0 {
         Ok(sz)
     } else {
-        Err(io::Error::new(io::ErrorKind::Other, "libc::getsockopt"))
+        Err(io::Error::other("libc::getsockopt"))
     }
 }
