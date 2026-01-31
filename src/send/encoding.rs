d6ee0f20d26c470017c2f0cc23d070632835610f
Arnaud Fontaine <33485997+github-af@users.noreply.github.com>
2026-01-31 22:14:56 +0100

    send: fix possible deadlock

--- src/send/encoding.rs
+++ src/send/encoding.rs
@@ -4,16 +4,16 @@ use crate::send;
 
 pub fn start<C>(sender: &send::Sender<C>) -> Result<(), send::Error> {
     loop {
-        let Some((block_id, block)) = sender.for_encoding.recv()? else {
+        let Some((id, block)) = sender.for_encoding.recv()? else {
             sender.to_send.send(None)?;
             return Ok(());
         };
 
         let client_id = block.client_id();
 
-        log::debug!("encoding block {block_id} for client {client_id:x}");
+        let packets = sender.raptorq.encode(id, block.serialized());
 
-        let packets = sender.raptorq.encode(block_id, block.serialized());
+        log::debug!("encoding block {id} for client {client_id:x}");
 
         let mut block_to_send = sender.block_to_send.0.lock().map_err(|e| {
             send::Error::Other(format!("failed to acquire block_to_send mutex: {e}"))
@@ -22,7 +22,7 @@ pub fn start<C>(sender: &send::Sender<C>) -> Result<(), send::Error> {
         block_to_send = sender
             .block_to_send
             .1
-            .wait_while(block_to_send, |block_to_send| *block_to_send != block_id)
+            .wait_while(block_to_send, |block_to_send| *block_to_send != id)
             .map_err(|e| {
                 send::Error::Other(format!("failed to wait_while block_to_send mutex: {e}"))
             })?;
