a558e3d3e7cfcf63bd2391cccc623edd0e96af55
Arnaud Fontaine <33485997+github-af@users.noreply.github.com>
2026-02-22 09:32:41 +0100

    send: remove ordering thread

--- src/send/heartbeat.rs
+++ src/send/heartbeat.rs
@@ -1,7 +1,7 @@
 //! Optional worker that periodically inserts [`crate::protocol`] heartbeat block in the encoding queue
 
 use crate::{protocol, send};
-use std::thread;
+use std::{sync, thread};
 
 pub fn start<C>(sender: &send::Sender<C>) -> Result<(), send::Error> {
     let Some(duration) = sender.config.heartbeat_interval else {
@@ -13,12 +13,14 @@ pub fn start<C>(sender: &send::Sender<C>) -> Result<(), send::Error> {
     loop {
         log::debug!("send heartbeat");
 
-        sender.to_ordering.send(Some(protocol::Block::new(
-            protocol::BlockType::Heartbeat,
-            &sender.raptorq,
-            0,
-            None,
-        )?))?;
+        let block_id = sender
+            .next_block
+            .fetch_add(1, sync::atomic::Ordering::SeqCst);
+
+        sender.to_udp.send(Some((
+            block_id,
+            protocol::Block::new(protocol::BlockType::Heartbeat, &sender.raptorq, 0, None)?,
+        )))?;
 
         thread::sleep(duration);
     }
